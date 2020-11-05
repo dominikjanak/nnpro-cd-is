8562984a867ead1c99f2bcf443c1caf2e18879f4
karliklukas <43926969+karliklukas@users.noreply.github.com>
2020-11-05 15:32:44 +0100

    Fix premise binding

--- src/main/java/cz/janakdom/backend/model/dto/PremiseIncidentDto.java
+++ src/main/java/cz/janakdom/backend/model/dto/PremiseIncidentDto.java
@@ -1,8 +1,10 @@
 package cz.janakdom.backend.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -10,5 +12,7 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PremiseIncidentDto {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Europe/Prague")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date valid = null;
 }
