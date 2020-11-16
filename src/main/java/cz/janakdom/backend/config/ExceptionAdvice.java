d12ebb484608808a487d010f07d1bbdc1c98e144
Dominik Janák <dev@dominikjanak.cz>
2020-11-16 14:11:45 +0100

    Fix CORS

--- src/main/java/cz/janakdom/backend/config/ExceptionAdvice.java
+++ src/main/java/cz/janakdom/backend/config/ExceptionAdvice.java
@@ -2,10 +2,12 @@ package cz.janakdom.backend.config;
 
 import cz.janakdom.backend.model.ApiResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@CrossOrigin
 @RestControllerAdvice
 public class ExceptionAdvice {
 
@@ -14,8 +16,8 @@ public class ExceptionAdvice {
     protected ApiResponse<Void> handleGlobalException(Exception ex) {
 
         if(ex.getMessage().endsWith("NOT-FOUND")) {
-            return new ApiResponse<Void>(HttpStatus.NOT_FOUND.value(), ex.getMessage(), null);
+            return new ApiResponse<>(HttpStatus.NOT_FOUND.value(), ex.getMessage(), null);
         }
-        return new ApiResponse<Void>(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), null);
+        return new ApiResponse<>(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), null);
     }
 }
