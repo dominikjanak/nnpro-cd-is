4603179cd498020fe13fc41dfc894f46e7876e83
Dominik Janák <dev@dominikjanak.cz>
2020-11-18 20:11:42 +0100

    Add datetime

--- src/main/java/cz/janakdom/backend/controller/MainController.java
+++ src/main/java/cz/janakdom/backend/controller/MainController.java
@@ -7,12 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import springfox.documentation.annotations.ApiIgnore;
 
+import java.util.Date;
+import java.util.TimeZone;
+
 @ApiIgnore
 @RequestMapping(value = "/")
 @Controller
 public class MainController {
     @GetMapping("/")
+
     public String index(Model model) {
+        model.addAttribute("datetime",  new Date().toString());
         return "index";
     }
 }
